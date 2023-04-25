3ce55191c0e0616a2fc1affef3f491fb5b4632aa
fatema-zahraa <drzahraaahmad@gmail.com>
2023-04-25 11:28:03 -0700

    2-print_alphabet_x10.c

--- 2-print_alphabet_x10.c
+++ 2-print_alphabet_x10.c
@@ -8,12 +8,12 @@ void print_alphabet_x10(void)
 {
 int i;
 int j;
-for (i = 1; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 {
-for (j = 97; i <= 122; j++)
+	for (j = 97; i <= 122; j++)
 {
-_putchar(j);
+	_putchar(j);
 }
-_putchar('\n');
+	_putchar('\n');
 }
 }
