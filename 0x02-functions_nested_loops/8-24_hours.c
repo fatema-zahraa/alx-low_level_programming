e6b90699db8450a087f459277adf4ddb11e3d3ba
fatema-zahraa <drzahraaahmad@gmail.com>
2023-04-27 06:05:16 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,33 +1,12 @@
 #include "main.h"
 
 /**
- * jack_bauer-prints all minutes in a day from 00:00
- * Return:0 if succeesful.
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-
-void jack_bauer(void)
+int main(void)
 {
-int i, j, k, m;
-	for (i = 0; i <= 2; i++)
-{
-	for (j = 0; j <= 9; j++)
-{
-	for (k = 0; k <= 5; k++)
-{
-for (m = 0; m <= 9; m++)
-{
-if (i == 2 && j > 3)
-{
-	break;
-}
-	_putchar(i + '0');
-	_putchar(j + '0');
-	_putchar(':');
-	_putchar(k + '0');
-	_putchar(m + '0');
-	_putchar('\n');
-}
-}
-}
-}
+jack_bauer();
+return (0);
 }
