9c2011c8a74463e9ac3c8991631a9e8c72cbf409
”fatema-zahraa” <drzahraaahmad@gmail.com>
2023-06-01 11:12:57 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void first(void) __attribute__ ((constructor));
+/**
+ * first - main is printed after sentence
+ * function excution
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
