b5678f63df56aad42f861ce295871944cec6cc98
”fatema-zahraa” <drzahraaahmad@gmail.com>
2023-06-01 11:25:01 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,11 @@
-global   main
-	  extern    printf
+part		.text
+	global   main
+	extern    printf
 main:
 	  mov   edi, massege
 	  mov   eax, eax
 	  call  printf
 	  mov   eax, 0
 	  return
-format: massege db `Hello, Holberton\n`,0
-
+part		.data
+massege db `Hello, Holberton\n`,0
