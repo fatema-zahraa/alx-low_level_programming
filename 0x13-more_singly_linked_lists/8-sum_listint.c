61d78169527efad36a974543e8528341d52e7b76
”fatema-zahraa” <drzahraaahmad@gmail.com>
2023-06-07 07:14:07 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -7,12 +7,14 @@
  */
 int sum_listint(listint_t *head)
 {
- int sum = 0;
- listint_t *last = head;
- while (temp)
- {
- sum += last->n;
- last = last->next;
- }
- return (sum);
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
 }
